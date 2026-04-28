df1ac15bbeca8dc483c8fdbba3913be13adb17e2
DiegoD <diegodiazdelaconcha23@gmail.com>
2026-04-28 15:23:14 -0600

    se agrego el link hacia la app dentro de la tienda apple

--- src/lib/appLinks.js
+++ src/lib/appLinks.js
@@ -2,5 +2,5 @@
  * Central store for app download URLs.
  * Update these two constants when the app is published and you have the final links.
  */
-export const APP_STORE_URL = 'https://apps.apple.com/app/zetio/id6744534851'
+export const APP_STORE_URL = 'https://apps.apple.com/mx/app/zetio/id6757622344'
 export const PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=com.zetio.app'
